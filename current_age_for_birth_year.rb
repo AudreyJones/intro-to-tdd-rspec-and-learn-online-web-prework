d22917b82faf336123cff71d07c4e43b4d64b66c
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-08 18:03:23 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,2 @@
-birth_year = 1989;
-current_year = 2003;
-
-current_age_for_birth_year(current_year,birth_year) = current_year - birth_year;
-
-puts current_age_for_birth_year;
+def current_age_for_birth_year
+end
